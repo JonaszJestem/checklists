bf637c6a0b2498dd2a24c68b54b51571b0d0d085
jwiacek <jonasz.wiacek@nokia.com>
2018-11-16 10:53:51 +0100

    added file bases database

--- src/test/java/com/jonaszwiacek/checklists/Controllers/ChecklistControllerTest.java
+++ src/test/java/com/jonaszwiacek/checklists/Controllers/ChecklistControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestPropertySource("classpath:application.properties")
 public class ChecklistControllerTest {
     private static final Pattern NUMBER_PATTTERN = Pattern.compile("[0-9]+");
     @Autowired
